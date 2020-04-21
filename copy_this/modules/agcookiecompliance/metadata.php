562aa8fd43a821227cb511202e2d280fcd1054cb
alexander <coding@aggrosoft.de>
2020-04-21 14:33:40 +0200

    Release 1.0.4

--- copy_this/modules/agcookiecompliance/metadata.php
+++ copy_this/modules/agcookiecompliance/metadata.php
@@ -7,7 +7,7 @@ $aModule = array(
     'title'        => '<img src="' . oxRegistry::getConfig()->getShopUrl() . 'modules/agcookiecompliance/out/img/logo.png" height="15" alt="Cookie Compliance" title="Cookie Compliance"> Cookie Compliance',
     'description'  => 'GDPR Cookie Compliance',
     'thumbnail'    => '',
-    'version'      => '1.0.3',
+    'version'      => '1.0.4',
     'author'       => 'Aggrosoft GmbH',
     'extend'      => array(
         'oxviewconfig' => 'agcookiecompliance/extensions/core/agcookiecompliance_oxviewconfig'
